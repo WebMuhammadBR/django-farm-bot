56fffee0104f51f27000a768b1440c240e129d6a
WebMuhammadBR <81002797+WebMuhammadBR@users.noreply.github.com>
2026-02-18 17:24:06 +0500

    Improve login page responsiveness for mobile users

--- query/urls.py
+++ query/urls.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
-from .views import farmer_report,home
+
+from .views import farmer_report, home, login_view
 
 urlpatterns = [
     path("", home, name="home"),
+    path("login/", login_view, name="login"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
     path("report/farmer/", farmer_report, name="farmer_report"),
 ]
